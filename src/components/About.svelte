a4c8e98bb5d183cf580211ea3249654225423f47
Manuel Duarte <Manuel Duarte>
2020-11-01 16:51:26 -0600

    Condicionales

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -2,8 +2,19 @@
     let someText = `Backend Developer at HardSoftDay`
     let count = 0;
 
+    let styles = {darkMode: false}
+    let stylesLight = {lightMode: false}
+
     function handleClick() {
         count += 1
+    }
+
+    function toggle() {
+        styles.darkMode = !styles.darkMode;
+        stylesLight.lightMode = !stylesLight.lightMode;
+
+        window.document.body.classList.toggle("dark-mode")
+        window.document.body.classList.toggle("light-mode")
     }
 
 </script>
@@ -17,10 +28,28 @@
 </style>
 
 <div class="About">
-    <p>
-        {someText}
-    </p>
 
-    <button on:click={handleClick}>Click {count === 0 ? '' : count}</button>
 
-</div>
+    {#if !styles.darkMode}
+        <p>
+            {someText}
+        </p>
+        <p>
+            <span>Light Mode</span>
+        </p>
+    {:else}
+
+        <button on:click={toggle}>LightMode</button>
+        <p>
+            <span>Dark Mode</span>
+        </p>
+
+    {/if}
+
+
+
+        <button on:click={handleClick}>Click {count === 0 ? '' : count}</button>
+        <button on:click={toggle}>DarkMode</button>
+
+
+ </div>
--- src/components/Text.svelte
+++ src/components/Text.svelte
@@ -2,8 +2,6 @@
     export let anotherText = "lorem ipsum"
 </script>
 
-
-
 <div class="Text">
     <p>{anotherText}</p>
 </div>
